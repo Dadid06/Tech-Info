84002abadc409a0cb21ab3dac359a68af583f314
houssine69410 <57896459+houssine69410@users.noreply.github.com>
2019-11-18 15:18:43 +0100

    ajouter commentaire reparation

--- reparation.php
+++ reparation.php
@@ -1,3 +1,55 @@
 <?php
 
-    echo 'reparation';
+   include 'header.php';
+
+?>
+    <!-- START THE FEATURETTES -->
+   <hr class="featurette-divider">
+   <div class="row featurette">
+     <div class="col-md-7">
+       <h2 class="featurette-heading">Réparation de smartphone tablette PC portable  <span class="text-muted">Réparez rapidement en toute confiance! </span></h2>
+       <p class="lead">Votre appareil ne fonctionne plus? Il est temps de l'apporter à nos techniciens spécialisés en réparation smartphone, tablettes et pc portable!</p>
+     </div>
+     <div class="col-md-5">
+       <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+     </div>
+   </div>
+   <hr class="featurette-divider">
+   <div class="row featurette">
+     <div class="col-md-7 order-md-2">
+       <h2 class="featurette-heading">Réparation de smartphone tablette PC portable  <span class="text-muted">Réparez rapidement en toute confiance! </span></h2>
+       <p class="lead">Nos réparateurs sont capables d'intervenir sur la plupart des smartphones (Apple, Samsung, Nokia, Sony, Condor, Huawei, HTC, LG, ...), des tablettes (Apple, Samsung, ...) et des ordinateurs portables (Apple, Lenovo, HP, Asus, ...). </p>
+     </div>
+     <div class="col-md-5 order-md-1">
+       <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+     </div>
+   </div>
+ 
+   <hr class="featurette-divider">
+   <!-- /END THE FEATURETTES -->
+ </div><!-- /.container -->
+    
+    
+ 
+    
+  
+     
+
+       
+           
+
+
+
+
+
+
+
+
+
+
+<?php
+
+    include 'footer.php';
+
+?>
+   
